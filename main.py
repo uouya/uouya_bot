b1e25167844938a405f341de50e6c6a88a825181
uouya <aonks159@gmail.com>
2025-04-20 03:40:34 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import discord
 from discord.ext import commands
 from discord import app_commands
@@ -5,7 +6,7 @@ from discord import app_commands
 from myserver import server_on
 bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
 
-TOKEN = 'MTM2Mjg1OTU1NTMxODUzNDE1NA.GSRTtN.TMQmkp99zV9L5xl3lr5PrXJwthuxmIQaLO1jTI'
+
 
 @bot.event
 async def on_ready():
@@ -13,4 +14,4 @@ async def on_ready():
 
     server_on()
 
-    bot.run(TOKEN)
+    bot.run(os.getenv(TOKEN))
